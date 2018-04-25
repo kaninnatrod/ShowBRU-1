fc11243f63ca96e7dcd9911ef9314546f2d3705f
MasterUNG <phrombutr@gmail.com>
2018-04-25 17:10:03 +0700

    Finish 25April

--- app/src/main/java/masterung/androidthai/in/th/showbru/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/showbru/fragment/RegisterFragment.java
@@ -4,13 +4,49 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.showbru.MainActivity;
 import masterung.androidthai.in.th.showbru.R;
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+
+//        Setup Navigation Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
+
 
     @Nullable
     @Override
