bd4bffd81192f7672ecc79bb9a05dd0dc3fdbd79
MasterUNG <phrombutr@gmail.com>
2018-04-26 15:04:52 +0700

    Start

--- app/src/main/java/masterung/androidthai/in/th/showbru/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/showbru/fragment/MainFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import masterung.androidthai.in.th.showbru.R;
+import masterung.androidthai.in.th.showbru.utility.GetAllData;
+import masterung.androidthai.in.th.showbru.utility.MyAlert;
+import masterung.androidthai.in.th.showbru.utility.MyConstant;
 
 public class MainFragment extends Fragment{
 
@@ -18,6 +24,66 @@ public class MainFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Register Controller
+        registerController();
+
+//        Login Controller
+        loginController();
+
+
+    }   // Main Method
+
+    private void loginController() {
+
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.normalDialog("Have Space",
+                            "Please Fill Every Blank");
+
+                } else {
+//                    No Space
+
+                    MyConstant myConstant = new MyConstant();
+                    boolean b = true;
+                    String truePass, nameUser;
+
+                    try {
+
+                        GetAllData getAllData = new GetAllData(getActivity());
+                        getAllData.execute(myConstant.getUrlGetAllUser());
+
+                        String jsonString = getAllData.get();
+                        Log.d("26AprilV1", "JSON ==> " + jsonString);
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
+
+                }   // if
+
+
+
+            }
+        });
+
+    }
+
+    private void registerController() {
         TextView textView = getView().findViewById(R.id.txtRegister);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +99,7 @@ public class MainFragment extends Fragment{
 
             }
         });
-
-
-    }   // Main Method
+    }
 
     @Nullable
     @Override
---
+++ app/src/main/java/masterung/androidthai/in/th/showbru/utility/MyConstant.java
@@ -0,0 +1,16 @@
+package masterung.androidthai.in.th.showbru.utility;
+
+public class MyConstant {
+
+    private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
+    private String urlGetAllUser = "http://androidthai.in.th/bru/getAllUser.php";
+
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
